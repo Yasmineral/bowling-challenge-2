83679a383bb4c03713657c72d5fdfc2e91b9306b
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-01-08 16:16:24 +0000

    fix ESLint errors

--- src/frame.js
+++ src/frame.js
@@ -1,13 +1,13 @@
 class Frame {
   constructor() {
-    this.score = []
+    this.score = [];
   }
 
   roll(totalPins) {
-    this.score.push(totalPins)
+    this.score.push(totalPins);
   }
 
   getScore() {
-    return this.score.reduce((a, b) => a + b) 
+    return this.score.reduce((a, b) => a + b);
   }
 }
