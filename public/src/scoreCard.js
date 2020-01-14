24fd5a09f48c189234b3eed847047c58abd9618a
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-01-14 11:39:59 +0000

    tweak the addScore array

--- public/src/scoreCard.js
+++ public/src/scoreCard.js
@@ -1,10 +1,20 @@
 class scoreCard {
   constructor() {
     this.allScores = []
+    this.currentFrame = 1
+  }
+ 
+  getTotalScore() {
+    var sum = 0;
+    for (var i = 0; i < this.allScores.length; i++) {
+      for (var j = 0; j < this.allScores[i].length; j++) {
+        sum += this.allScores[i][j];
+      }
+    }
+    return sum
   }
 
   addScore(frame) {
-    this.allScores.push(frame.frameScore)
+    return this.allScores.push(frame.frameScore)
   }
-   
 }
--- spec/scoreCardSpec.js
+++ spec/scoreCardSpec.js
@@ -6,9 +6,20 @@ describe ("scoreCard", function() {
     var frame = {
       frameScore: [4,5]
     }
-    it("adds the scores from both rolls of the frame", function() {
+    it("adds an array of scores to the main allScores array", function() {
       scoreCard.addScore(frame)
-      expect(scoreCard.allScores).toEqual([[4,5]])
+      expect(scoreCard.allScores[0]).toEqual([4,5])
     })
   })
+  // describe("#spareBonus", function() {
+  //   var frame1 = {
+  //     frameScore: [5,5]
+  //   }
+  //   var frame2 = {
+  //     frameScore: [5, 0]
+  //   }
+  //   it("total from the next roll is added to the total from the last frame", function() {
+  //     expect(scoreCard.getTotalScore()).toEqual(20)
+  //   })
+  // })
 })
